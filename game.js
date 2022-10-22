19ceea85f6c1b24217ea1515c534ad910bf14cbe
Jeremy Martin <jeremy.mrti@gmail.com>
2022-10-22 15:29:04 +0200

    For loop working and 5 rounds are ok

--- game.js
+++ game.js
@@ -11,29 +11,31 @@
  console.log(computerSelection)
 
  // The player makes a selection, capitalized only on first letter
- let playerChoice = prompt("What will your choice be ?")
- function capitalizeChoice() {
+ function getPlayerChoice () {
+    let playerChoice = prompt("What will your choice be ?")
     return (playerChoice.charAt(0).toUpperCase() + playerChoice.slice (1).toLowerCase())
 }
-let playerSelection = capitalizeChoice()
+
+let playerSelection = getPlayerChoice()
 console.log(playerSelection)
 
 // Game is ON !
 function playRound(playerSelection, computerSelection) {
     // If both variables matches, it's a tie (using switch, this would be the default.. ?)
     if (playerSelection === computerSelection) {
-        return ("It's a tie !")
+        return (null)
     } else {
         if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-            return ("You win !")
+            return (true)
             } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-                return ('You win !')
+                return (true)
             } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-                return ('You win !')
+                return (true)
             } else {
-                return ('You loose')
+                return (false)
             }
     }
+
     // If not, then : 
     // If Player is Paper and Computer is Rock, then it's a win
         // If computer is scissors, it's a loose
@@ -41,5 +43,45 @@ function playRound(playerSelection, computerSelection) {
             // if comptuer is scissors, it's a win
     // If player is scissors and computer paper, it's a win
             // if computer is rock, its a loose
+
 }
 console.log (playRound(playerSelection,computerSelection))
+
+// Getting to 5 games in a row
+        let computerScore  
+        let playerScore
+
+function game() {
+
+
+    for (let i = 0 ; i < 4 ; i++) {
+        
+        
+        let computerSelection = getComputerChoice();     
+       console.log(computerSelection) ;
+        let playerSelection = getPlayerChoice() ;
+        console.log(playerSelection) ;
+
+        let result = playRound(playerSelection, computerSelection) ;
+        console.log(result) ;
+    }
+    //if (result === true) {
+            
+    //      return ('You win !')
+    // } else if (result === false) {
+         
+    //        return ('You loose...')
+    //    } else if (result === null ) {
+    //        return ('It\'s a tie')
+    //    }
+
+
+        // Keeping score by creating 2 variables for computer and player
+        // That variable gets increased every win, until it reaches 3
+
+        // After each iteration, console.log the current score
+        // Winner gets declared
+
+}        
+
+ game()
