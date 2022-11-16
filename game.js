b68435548851df029901b29941348d2fe1cbd85f
Jeremy Martin <jeremy.mrti@gmail.com>
2022-11-16 11:48:55 +0100

    Adding of EventListener and buttons working For one round only

--- game.js
+++ game.js
@@ -1,87 +1,84 @@
-// The Computer chooses an option randomly
- function getComputerChoice() {
+const buttons = document.querySelectorAll('button');
+
+function getComputerChoice() {
     const choices = ['Paper', 'Rock','Scissors']
     var computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    console.log(computerChoice)
     return (computerChoice)
  }
-// The option is stored in a variable (not a const (to be verified?)) Scope to be verified as well
 
- //var computerSelection = getComputerChoice() --  Used before game()
- //console.log(computerSelection) --  Used before game()
+buttons.forEach((button) => {
+   button.addEventListener('click', (e) => {
+      console.log(button.id)
+      playRound(button.id, getComputerChoice());
+})
+})
 
- // The player makes a selection, capitalized only on first letter
- function getPlayerChoice () {
-    var playerChoice = prompt("What will your choice be ?")
-    if (playerChoice===null) {
-        console.log('Giving up already ?')
-      
-    }
-    return (playerChoice.charAt(0).toUpperCase() + playerChoice.slice (1).toLowerCase())
-
-}
-
-//var playerSelection = getPlayerChoice() --  Used before game()
-//console.log(playerSelection) --  Used before game()
-
-// Game is ON !
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return (null)
-    } else {
-        if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-            return (true)
-            } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-                return (true)
-            } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-                return (true)
-            } else {
-                return (false)
-            }
-    }
-}
-//console.log (playRound(playerSelection,computerSelection)) --  Used before game()
-
-// Getting to 5 games in a row
-
-function game() {
-
-        var computerScore = 0 ; 
-        var playerScore = 0 ;
-
-    for (let i = 0 ; i < 5 ; i++) {
-        
-        let playerSelection = getPlayerChoice() ;
-        console.log('You chose ' + playerSelection) ;
-
-        let computerSelection = getComputerChoice();     
-       console.log('The computer chooses ' + computerSelection) ;
-
-
-        var result = playRound(playerSelection, computerSelection) ;
-        console.log(result) ;
-
-        if (result===true) {
-            playerScore++;
-            console.log('Well played !')
-        } else if (result===false) {
-            computerScore++;
-            console.log('Bad choices happen..')
-        } else if (result===null) {
-            console.log('It\s a tie.')
-        }
+function playRound(playerSelection,computerSelect) {
  
-        console.log('Computer score is '  + computerScore)
-        console.log('Your score is ' + playerScore)
+   if (playerSelection === computerSelect) {
+         console.log('nul');
+         return null
+      } else {
+         if (playerSelection === 'Rock' && computerSelect === 'Scissors') {
+         console.log('win');   
+         return true
+           } else if (playerSelection === 'Paper' && computerSelect === 'Rock') {
+            console.log('win');     
+          return true
+           } else if (playerSelection === 'Scissors' && computerSelect === 'Paper') {
+            console.log('win');     
+            return true
+           } else {
+            console.log('lose');     
+           return true
+           }
+   }
+      };
+
+
+
+
+// function game() {
+
+//         var computerScore = 0 ; 
+//         var playerScore = 0 ;
+
+//     for (let i = 0 ; i < 5 ; i++) {
         
-    }
+//         let playerSelection = getPlayerChoice() ;
+//         console.log('You chose ' + playerSelection) ;
 
-    if (computerScore > playerScore) {
-        console.log('You loose by ' + (computerScore-playerScore) + (' point(s)... harsh !'))
-    } else if (computerScore < playerScore) {
-        console.log('You win !! You defeated this poor computer by ' + (playerScore-computerScore) + ' point(s)')
-    } else if (computerScore === playerScore) {
-        console.log('It\'s a tie all the way. No luck !')
-    }
-} 
+//         let computerSelection = getComputerChoice();     
+//        console.log('The computer chooses ' + computerSelection) ;
 
- game()    
+
+//         var result = playRound(playerSelection, computerSelection) ;
+//         console.log(result) ;
+
+//         if (result===true) {
+//             playerScore++;
+//             console.log('Well played !')
+//         } else if (result===false) {
+//             computerScore++;
+//             console.log('Bad choices happen..')
+//         } else if (result===null) {
+//             console.log('It\s a tie.')
+//         }
+ 
+//         console.log('Computer score is '  + computerScore)
+//         console.log('Your score is ' + playerScore)
+        
+//     }
+
+//     if (computerScore > playerScore) {
+//         console.log('You loose by ' + (computerScore-playerScore) + (' point(s)... harsh !'))
+//     } else if (computerScore < playerScore) {
+//         console.log('You win !! You defeated this poor computer by ' + (playerScore-computerScore) + ' point(s)')
+//     } else if (computerScore === playerScore) {
+//         console.log('It\'s a tie all the way. No luck !')
+//     }
+// } 
+
+//  game()    
+
